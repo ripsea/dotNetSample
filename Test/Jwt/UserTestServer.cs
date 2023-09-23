d610b21dfc415096997d146cc638f43214dcd7b0
iris <ripsea0505@hotmail.com>
2023-09-23 11:17:51 +0800

    feat: create WebApplicationFactory in Jwt Test Project

---
+++ Test/Jwt/UserTestServer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using NSubstitute;
+using Services.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtTest
+{
+    internal class UserTestServer : WebApplicationFactory<Program>
+    {
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped(p =>
+            {
+                var userService = Substitute.For<IUserServiceRepository>();
+                userService.AddUserRefreshTokens().RefreshToken
+                return userService;
+            });
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(this.ConfigureServices);
+        }
+    }
+}
---
+++ Test/Jwt/UserTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Jwt
+{
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WebApplicationFactory基本用法()
+        {
+            var server = new WebApplicationFactory<Program>();
+            var client = server.CreateClient();
+            var url = "api/users";
+            var response = client.GetAsync(url).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+        }
+    }
+}
