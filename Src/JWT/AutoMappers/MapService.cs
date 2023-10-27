b8c9f9549d032f5dafb478b84e6cc4d857292257
iris <ripsea0505@hotmail.com>
2023-10-27 10:42:03 +0800

    doc: tmp

--- Src/JWT/AutoMappers/MapService.cs
+++ Src/JWT/AutoMappers/MapService.cs
@@ -5,6 +5,20 @@ public class MapService: IMapService
 {
     private readonly IMapper _mapper;
 
+    //https://igouist.github.io/post/2020/07/automapper/
+    //註：網路上挺多 AutoMapper 的文章會直接使用 Mapper.CreateMap() 這類靜態方法，
+    //然而這些方法已經在 AutoMapper 5 的時候被廢除，改
+    //成現在由 MapperConfiguration 產生 Mapper 的方法。
+
+    //首先我們先建立一個用來放對映關係的 Profile。
+    //我個人習慣會另開一個 Mappings 資料夾，並按照轉換所在的分層 + Mappings 來命名。
+    //以此例, ServiceMappings為供Service功能的mapper設定
+
+    //將 CreateMap 都整理到 Profile 之後，若是有些類別之間的轉換有對欄位做額外的處理，
+    //例如 DateTime 去除時間只留下年月日，又或是某幾個欄位銜接成一個欄位等等，
+    //在實際進行類別轉換的時候需要註記一下，請後續的維護人員或團隊夥伴記得先確認過 Profile，
+    //避免造成一些隱藏重要資訊挖洞給人跳的問題。
+
     public MapService()
     {
         var config = new MapperConfiguration(
--- Src/JWT/AutoMappers/ServiceMappings.cs
+++ Src/JWT/AutoMappers/ServiceMappings.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Data.Entities.Auth;
 using Jwt.Models;
 using JWT.Models;
 using Services.Models;
@@ -8,7 +9,16 @@ public class ServiceMappings : Profile
     public ServiceMappings()
     {
         CreateMap<User, LoginViewModel>()
+            //它能夠反轉對映，建立一個反方向的對映表。
+            //當你的類別之間可能需要往回轉型，又或是想要忽略來源類別的欄位時，
+            //ReverseMap 就會顯得相當有用
             .ReverseMap();
+
+        CreateMap<User, ApplicationUser>()
+            .ForMember(y => y.RefreshToken, x => x.MapFrom(o => o.RefreshToken))
+            .ForMember(y => y.RefreshTokenExpiryTime, x => x.MapFrom(o => o.RefreshTokenExpiryTime));
+
+        
 
         // ...其他的對映內容 (使用 CreateMap<> 建立下一組)
     }
